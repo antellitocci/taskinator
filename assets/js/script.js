4446111512a13423edb8136e3905bf21637afecf
raddymaster <antellitocci@gmail.com>
2021-05-01 12:13:41 -0400

    refactored createTaskHandler and add functionality to include task type when a new task is added to task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,22 @@ var createTaskHandler = function(event){
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //Create a list item
     var taskListItemElem = document.createElement('li');
     taskListItemElem.className = "task-item";
-    taskListItemElem.textContent = "I wish I could submit my own task.";
+
+    //Create a div to hold task info and add to list item
+    var taskInfoElem = document.createElement('div');
+    //Give it a class name
+    taskInfoElem.className = "task-info";
+    //Add HTML content to div
+    taskInfoElem.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskListItemElem.appendChild(taskInfoElem);
+
+    //Add entire list item to list
     tasksToDoElem.appendChild(taskListItemElem);
 }
 
